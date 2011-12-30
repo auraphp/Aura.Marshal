356bef19cd447d291ca1bdaa294da875c025f0c6
Paul M. Jones <pmjones88@gmail.com>
2011-12-29 18:08:39 -0600

    changed from loader addPrefix() to just add()

--- config/default.php
+++ config/default.php
@@ -2,7 +2,7 @@
 /**
  * Package prefix for autoloader.
  */
-$loader->addPrefix('Aura\Marshal\\', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src');
+$loader->add('Aura\Marshal\\', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src');
 
 $di->set('marshal_manager', function() use ($di) {
     return $di->newInstance('Aura\Marhal\Manager', array(
