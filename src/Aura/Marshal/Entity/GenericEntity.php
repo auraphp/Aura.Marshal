907e93a2e08a73f0f24223dbcf74dcf92694d883
Jake Smith <jake.smith92@gmail.com>
2014-01-29 16:46:27 -0600

    Change GenericEntity to properly be converted to an array via iterator_to_array

--- src/Aura/Marshal/Entity/GenericEntity.php
+++ src/Aura/Marshal/Entity/GenericEntity.php
@@ -1,25 +1,39 @@
 <?php
 /**
- * 
+ *
  * This file is part of the Aura project for PHP.
- * 
+ *
  * @package Aura.Marshal
- * 
+ *
  * @license http://opensource.org/licenses/bsd-license.php BSD
- * 
+ *
  */
 namespace Aura\Marshal\Entity;
 
 use Aura\Marshal\Data;
 
 /**
- * 
+ *
  * Represents a single entity.
- * 
+ *
  * @package Aura.Marshal
- * 
+ *
  */
 class GenericEntity extends Data
 {
     use MagicArrayAccessTrait;
+
+    /**
+     *
+     * ArrayAccess: get a key value.
+     *
+     * @param int|string $key The requested key.
+     *
+     * @return mixed
+     *
+     */
+    public function offsetGet($key)
+    {
+        return $this->$key;
+    }
 }
--- src/Aura/Marshal/Entity/MagicArrayAccessTrait.php
+++ src/Aura/Marshal/Entity/MagicArrayAccessTrait.php
@@ -1,47 +1,46 @@
 <?php
 /**
- * 
+ *
  * This file is part of the Aura project for PHP.
- * 
+ *
  * @package Aura.Marshal
- * 
+ *
  * @license http://opensource.org/licenses/bsd-license.php BSD
- * 
+ *
  */
 namespace Aura\Marshal\Entity;
 
 use Aura\Marshal\Lazy\LazyInterface;
 
 /**
- * 
+ *
  * Use this trait for magic __get(), __set(), __isset(), and __unset() on any
  * entity class that implements ArrayAccess for its fields.
- * 
+ *
  * @package Aura.Marshal
- * 
+ *
  */
 trait MagicArrayAccessTrait
 {
     /**
-     * 
+     *
      * Calls ArrayAccess::offsetGet() to get a field value; converts Lazy
      * objects for related entities and collections in place.
-     * 
+     *
      * @param string $field The field name to get.
-     * 
+     *
      * @return mixed The field value.
-     * 
+     *
      */
     public function __get($field)
     {
         // get the field value
-        $value = $this->offsetGet($field);
+        $value = $this->data[$field];
 
         // is it a Lazy placeholder?
         if ($value instanceof LazyInterface) {
             // replace the Lazy placeholder with the real object
             $value = $value->get($this);
-            $this->offsetSet($field, $value);
         }
 
         // done!
@@ -49,15 +48,15 @@ trait MagicArrayAccessTrait
     }
 
     /**
-     * 
+     *
      * Calls ArrayAccess::offsetSet() to set a field value.
-     * 
+     *
      * @param string $field The field name to get.
-     * 
+     *
      * @param mixed $value Set the field to this value.
-     * 
+     *
      * @return void
-     * 
+     *
      */
     public function __set($field, $value)
     {
@@ -65,13 +64,13 @@ trait MagicArrayAccessTrait
     }
 
     /**
-     * 
+     *
      * Calls ArrayAccess::offsetExists() to see if a field is set.
-     * 
+     *
      * @param string $field The field name to check.
-     * 
+     *
      * @return bool
-     * 
+     *
      */
     public function __isset($field)
     {
@@ -79,13 +78,13 @@ trait MagicArrayAccessTrait
     }
 
     /**
-     * 
+     *
      * Calls ArrayAccess::offsetUnset() to unset a field.
-     * 
+     *
      * @param string $field The field name to unset.
-     * 
+     *
      * @return void
-     * 
+     *
      */
     public function __unset($field)
     {
